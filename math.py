5407f7bedfa5ac240f2765792877205d9f597d08
ebarnas <113565846+ebarnas@users.noreply.github.com>
2024-01-29 13:41:08 -0600

    Add files via upload

---
+++ math.py
@@ -0,0 +1,6 @@
+#Using Mathematical Operations in Program
+
+length= 10
+width= 5
+area= length * width
+print("The area of a rectangle with a length of " + str(length) + " and a width of " + str(width) + " is " + str(area) + ".")
