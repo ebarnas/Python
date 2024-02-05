ec266c45e52f5eca2294d6fd8d4576b5322e1286
ebarnas <113565846+ebarnas@users.noreply.github.com>
2024-02-05 11:30:59 -0600

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,14 @@
+#This is the tank size of the car, of how much fuel it can hold
+tank_size = 15
+
+#This is the distance that the car is driving
+distance = 600
+
+#This is an equation for miles per gallon
+#This takes the tank size and the distance
+#The distance divides the tank size and that is how you get the miles per gallons
+mpg = distance / tank_size
+
+#This will state the words 'miles per gallon' 
+#Along with the number from the equation
+print("Miles per gallon:", mpg)
