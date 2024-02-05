99ba3d896f85fc9deb1a84d586b58dd681a13d28
ebarnas <113565846+ebarnas@users.noreply.github.com>
2024-02-05 11:11:38 -0600

    Add files via upload

---
+++ kilograms.py
@@ -0,0 +1,17 @@
+#Converting Kilograms(kg) to Pounds(lbs)
+kg_value_1 = 10.358
+kg_value_2 = 54.211
+kg_value_3 = 5.248
+kg_value_4 = 24.984
+
+conversion_factor = 2.20462
+
+lbs_1 = kg_value_1 * conversion_factor
+lbs_2 = kg_value_2 * conversion_factor
+lbs_3 = kg_value_3 * conversion_factor
+lbs_4 = kg_value_4 * conversion_factor
+
+print(f"{kg_value_1} kilograms is equal to {lbs_1:.2f} pounds.")
+print(f"{kg_value_2} kilograms is equal to {lbs_2:.2f} pounds.")
+print(f"{kg_value_3} kilograms is equal to {lbs_3:.2f} pounds.")
+print(f"{kg_value_4} kilograms is equal to {lbs_4:.2f} pounds.")
