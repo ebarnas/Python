7683348b8d0f229f54f90f1c59d9472fbb1b7a6a
ebarnas <113565846+ebarnas@users.noreply.github.com>
2024-04-29 13:14:28 -0500

    Add files via upload

---
+++ MyFiles/file_organizer.py
@@ -0,0 +1,9 @@
+def main():
+    #This is organizers files together and under one main file
+    import os
+
+    os.mkdir("MyFiles")
+    os.mkdir("MyFiles/Docs")
+    os.mkdir("MyFiles/Images")
+    os.mkdir("MyFiles/Videos")
+main()
