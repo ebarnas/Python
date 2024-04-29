46d17d1792ac9008619765226e579aa7a1789c4b
ebarnas <113565846+ebarnas@users.noreply.github.com>
2024-04-29 13:12:36 -0500

    Delete file_organizer.py

--- file_organizer.py
+++
@@ -1,9 +0,0 @@
-def main():
-    #This is organizers files together and under one main file
-    import os
-
-    os.mkdir("MyFiles")
-    os.mkdir("MyFiles/Docs")
-    os.mkdir("MyFiles/Images")
-    os.mkdir("MyFiles/Videos")
-main()
